aa32852d09a008f89e460cfb02ce3a88c829e23b
Lewis Barber <lewis.barber@sharpecapital.co.uk>
2017-06-24 21:22:44 +0100

    TDD placeholders

--- test/sharpe-contribution.js
+++ test/sharpe-contribution.js
@@ -216,4 +216,41 @@ contract("SharpeContribution", function(accounts) {
         assertBalances.ether(10, 0, 80, 100, 0, 0);
         await assertBalances.SHP(0, 0, 20000, 0, 20000, 10000);
     });
+
+    // TODO - there will be a known address that must make the first contribution...
+    // After which, all other contributions will be allowed (for a fixed time period)
+    it('should only allow master address to start contribution phase', async function() {});
+    it('should open contributions for everyone after known address is used', async function() {});
+    it('should not allow any more contributions from master address after used to start contribution phase', async function(){});
+    it('should not allow pausing when not owner of contract', async function() {});
+    it('should not allow resuming when not owner of contract', async function() {});
+    it('should not allow contributions when contract is not initialized', async function() {});
+    it('should only allow the owner of the SharpeToken contract to mint SHP tokens', async function() {});
+
+    // Limits
+    it('should reject contributions greater than the maximum ETH deposit', async function() {});
+    it('should reject contributions if SHP would exceed max supply limit', async function() {});
+
+    // Vesting - reserve
+    it('should reject transfer of SHP from reserve account before 12 month vesting period', async function() {});
+    it('should allow transfer of SHP from reserve account after 12 month vesting period', async function() {});
+    it('should allow reserve SHP to be transferred with multiple signatures', async function() {});
+    it('should reject reserve SHP transfer without multiple signatures', async function() {});
+
+    // Vesting - founders
+    it('should reject transfer of founders SHP tokens before 6 month vesting period', async function() {});
+    it('should allow transfer of 25% of founders SHP tokens after 6 month vesting period', async function() {});
+    it('should reject transfer of 75% of founders SHP tokens before 12 month vesting period', async function() {});
+    it('should allow transfer of 75% of founders SHP tokens after 12 month vesting period', async function() {});
+    it('should allow founders SHP to be transferred with multiple signatures', async function() {});
+    it('should reject founders SHP transfer without multiple signatures', async function() {});
+
+    // Vesting - contributors
+    it('should reject transfer of contributors SHP tokens before contribution period ends', async function() {});
+    it('should allow transfer of contributors SHP tokens after contribution period ends', async function() {});
+
+    it('should allow ETH to be transferred after contribution period closes', async function() {});
+    it('should reject ETH transfer before contribution period closes', async function() {});
+    it('should allow ETH to be transferred with multiple signatures', async function() {});
+    it('should reject ETH transfer without multiple signatures', async function() {});
 });
