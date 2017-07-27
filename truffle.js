eaff1aed2e7e73c465250d29590ee2a9d309376b
Lewis Barber <lewis.barber@sharpecapital.co.uk>
2017-07-27 19:25:10 +0100

    Remove unused code

--- truffle.js
+++ truffle.js
@@ -1,9 +1,3 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-const mnemonic = process.env.TEST_MNEMONIC || 'status mnemonic status mnemonic status mnemonic status mnemonic status mnemonic status mnemonic';
-const providerRopsten = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/', 0);
-const providerKovan = new HDWalletProvider(mnemonic, 'https://kovan.infura.io', 0);
-
 module.exports = {
     rpc: {
         host: "localhost",
