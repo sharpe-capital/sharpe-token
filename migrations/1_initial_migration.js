e966d67487f52f7d8848259c3bb674b870dca624
Lewis Barber <lewis_barber@hotmail.com>
2017-11-18 13:11:22 +0000

    Fixed deploy scripts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,64 +1,8 @@
 var Migrations = artifacts.require("./Migrations.sol");
-var SharpeCrowdsale = artifacts.require("./SharpeCrowdsale.sol");
-var abi = require('ethereumjs-abi');
 
 module.exports = async function(deployer, network, accounts) {
-
     if(network == "coverage" || network == "development") {
         return;
     }
-
-    const ethRate = 300;
-    const etherEscrowAddress = "0x28350c8ce237bc205b990a40266b46fe65197db4";
-    const bountyAddress = "0x762f11b6509fbb0f7a90b5ba80d3add087bebff5";
-    const trusteeAddress = "0x6d15a5f9c98e272686a5b241bc31516514e18b30";
-    const minDiscount = web3.toWei(1500 / ethRate);
-    const firstTierDiscount = web3.toWei(10000 / ethRate);
-    const secondTierDiscount = web3.toWei(50000 / ethRate);
-    const thirdTierDiscount = web3.toWei(250000 / ethRate);
-    const minContribution = web3.toWei(100 / ethRate);
-    const maxContribution = web3.toWei(500000 / ethRate);
-    const shpRate = 5000;
-
-    this.sharpeCrowdsale = await SharpeCrowdsale.new(
-        etherEscrowAddress,
-        bountyAddress,
-        trusteeAddress,
-        minDiscount,
-        firstTierDiscount,
-        secondTierDiscount,
-        thirdTierDiscount,
-        minContribution,
-        maxContribution,
-        shpRate
-    );
-
-    console.log('Deployed the Sharpe crowdsale: ' + this.sharpeCrowdsale.address);
-
-    console.log("Crowdsale ABI arguments: ", 
-    abi.rawEncode([
-        "address",
-        "address",
-        "address",
-        "uint256",
-        "uint256",
-        "uint256",
-        "uint256",
-        "uint256",
-        "uint256",
-        "uint256"
-    ], [
-        etherEscrowAddress,
-        bountyAddress,
-        trusteeAddress,
-        minDiscount,
-        firstTierDiscount,
-        secondTierDiscount,
-        thirdTierDiscount,
-        minContribution,
-        maxContribution,
-        shpRate
-    ]).toString('hex'));
-
     deployer.deploy(Migrations);
 };
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,60 +1,54 @@
 // Contracts:
-// const MiniMeTokenFactory = artifacts.require("MiniMeTokenFactory");
-// const MiniMeToken = artifacts.require("./MiniMeToken.sol");
-// const SharpeCrowdsale = artifacts.require("./SharpeCrowdsale.sol");
-// const SHP = artifacts.require("./SHP.sol");
-// const SCD = artifacts.require("./SCD.sol");
-// const AffiliateUtility = artifacts.require("./AffiliateUtility.sol");
-// const DynamicCeiling = artifacts.require("./DynamicCeiling.sol");
-// const SharpePresale = artifacts.require("./SharpePresale.sol");
-// const SHPController = artifacts.require("./SHPController.sol");
-// const TokenSale = artifacts.require("./TokenSale.sol");
-// const Owned = artifacts.require("./Owned.sol");
-// const SafeMath = artifacts.require("./SafeMath.sol");
-// const Trustee = artifacts.require("./Trustee.sol");
-// const MultiSigWallet = artifacts.require("./MultiSigWallet");
+const MiniMeTokenFactory = artifacts.require("MiniMeTokenFactory");
+const MiniMeToken = artifacts.require("./MiniMeToken.sol");
+const SharpeCrowdsale = artifacts.require("./SharpeCrowdsale.sol");
+const SHP = artifacts.require("./SHP.sol");
+const SHPController = artifacts.require("./SHPController.sol");
+const TokenSale = artifacts.require("./TokenSale.sol");
+const Owned = artifacts.require("./Owned.sol");
+const SafeMath = artifacts.require("./SafeMath.sol");
+const Trustee = artifacts.require("./Trustee.sol");
+const MultiSigWallet = artifacts.require("./MultiSigWallet");
 
 // //Keys
-// const Keys = require("./keys.json");
+const Keys = require("./testnet-keys.json");
 
 // // ABI GENERATOR
-// var abi = require('ethereumjs-abi')
+var abi = require('ethereumjs-abi');
 
 // // Addresses
-// const masterAddress = Keys.master; 
-// const apiAddress = Keys.api;
-// const bountyAddresses = [Keys.bounty.i,Keys.bounty.j,Keys.bounty.l];
-// const bountyRequiredSigs = Keys.bounty.req;
-// const ethAddresses = [Keys.ether.i,Keys.ether.j,Keys.ether.l,Keys.ether.taas];
-// const ethRequiredSigs = Keys.ether.req;
-// const foundersAddresses = [Keys.founders.i,Keys.founders.j,Keys.founders.l];
-// const foundersRequiredSigs = Keys.founders.req;
-// const reserveAddresses = [Keys.reserve.i,Keys.reserve.j,Keys.reserve.l,Keys.reserve.taas];
-// const reserveRequiredSigs = Keys.reserve.req;
-
-
+const masterAddress = Keys.master; 
+const apiAddress = Keys.api;
+const bountyAddresses = [Keys.bounty.i,Keys.bounty.j,Keys.bounty.l];
+const bountyRequiredSigs = Keys.bounty.req;
+const ethAddresses = [Keys.ether.i,Keys.ether.j,Keys.ether.l,Keys.ether.taas];
+const ethRequiredSigs = Keys.ether.req;
+const foundersAddresses = [Keys.founders.i,Keys.founders.j,Keys.founders.l];
+const foundersRequiredSigs = Keys.founders.req;
+const reserveAddresses = [Keys.reserve.i,Keys.reserve.j,Keys.reserve.l,Keys.reserve.taas];
+const reserveRequiredSigs = Keys.reserve.req;
 
 /////////
 //// Live Values
 ////////
 // Ether to Dollar Value
-const etherPeggedValue = 300;
+// const etherPeggedValue = 300;
 
-// Affiliate Tiers
-const affiliateTierTwo = web3.toWei(1) * (20000 / etherPeggedValue);
-const affiliateTierThree = web3.toWei(1) * (50000 / etherPeggedValue);
+// // Affiliate Tiers
+// const affiliateTierTwo = web3.toWei(1) * (20000 / etherPeggedValue);
+// const affiliateTierThree = web3.toWei(1) * (50000 / etherPeggedValue);
 
-//PRESALE VALUSE:
-const MIN_PRESALE_CONTRIBUTION = 10000;
-const MAX_PRESALE_CONTRIBUTION = 500000;
-const FIRST_TIER_DISCOUNT_UPPER_LIMIT = 49999;
-const SECOND_TIER_DISCOUNT_UPPER_LIMIT = 249999;
-const THIRD_TIER_DISCOUNT_UPPER_LIMIT = 500000;
-const PRESALE_CAP = 0;
-const honourWhitelistEnd = new Date(2017, 10, 8, 13, 59, 59, 0).getTime();
+// //PRESALE VALUSE:
+// const MIN_PRESALE_CONTRIBUTION = 10000;
+// const MAX_PRESALE_CONTRIBUTION = 500000;
+// const FIRST_TIER_DISCOUNT_UPPER_LIMIT = 49999;
+// const SECOND_TIER_DISCOUNT_UPPER_LIMIT = 249999;
+// const THIRD_TIER_DISCOUNT_UPPER_LIMIT = 500000;
+// const PRESALE_CAP = 0;
+// const honourWhitelistEnd = new Date(2017, 10, 8, 13, 59, 59, 0).getTime();
 
-//GENEARL SALE VALUES
-const MIN_GENERAL_SALE_CONTRIBUTION = 100;
+// //GENEARL SALE VALUES
+// const MIN_GENERAL_SALE_CONTRIBUTION = 100;
 
 
 /////////
@@ -82,192 +76,153 @@ const MIN_GENERAL_SALE_CONTRIBUTION = 100;
 
 module.exports = async function(deployer, network, accounts) {
 
-    // console.log("ADRESSES");
-    // console.log("------------------------------")
-    // console.log("masterAddress", masterAddress);
-    // console.log("bountyAddresses", bountyAddresses);
-    // console.log("bountyRequiredSigs", bountyRequiredSigs);
-    // console.log("ethAddresses", ethAddresses);
-    // console.log("ethRequiredSigs", ethRequiredSigs);
-    // console.log("foundersAddresses", foundersAddresses);
-    // console.log("foundersRequiredSigs", foundersRequiredSigs);
-    // console.log("reserveAddresses", reserveAddresses);
-    // console.log("reserveRequiredSigs", reserveRequiredSigs);
-    // console.log("------------------------------")
+    console.log("ADRESSES");
+    console.log("------------------------------")
+    console.log("masterAddress", masterAddress);
+    console.log("bountyAddresses", bountyAddresses);
+    console.log("bountyRequiredSigs", bountyRequiredSigs);
+    console.log("ethAddresses", ethAddresses);
+    console.log("ethRequiredSigs", ethRequiredSigs);
+    console.log("foundersAddresses", foundersAddresses);
+    console.log("foundersRequiredSigs", foundersRequiredSigs);
+    console.log("reserveAddresses", reserveAddresses);
+    console.log("reserveRequiredSigs", reserveRequiredSigs);
+    console.log("------------------------------")
 
-    // console.log("__________________________________________")
-    // console.log("*************Begin Migration**************")
-    // console.log("__________________________________________")
-    // console.log("deploying on network: " + network);
+    console.log("__________________________________________")
+    console.log("*************Begin Migration**************")
+    console.log("__________________________________________")
+    console.log("deploying on network: " + network);
 
-    // var parameterTypes = [];
-    // var parameterValues = [];
+    var parameterTypes = [];
+    var parameterValues = [];
     
-    // if(network == "coverage" || network == "development") {
-    //     return;
-    // }
+    if(network == "coverage" || network == "development") {
+        return;
+    }
 
-    // //SHP Token
-    // let miniMeTokenFactory = await MiniMeTokenFactory.new();
-    // let shp = await SHP.new(miniMeTokenFactory.address);
-    // let shpAddress = await shp.address;
-    // console.log("SHP" + " has been deployed to: " + shpAddress);
+    //SHP Token
+    let miniMeTokenFactory = await MiniMeTokenFactory.new();
+    let shp = await SHP.new(miniMeTokenFactory.address);
+    let shpAddress = await shp.address;
+    console.log("SHP" + " has been deployed to: " + shpAddress);
 
-    // //General Setup
-    // let etherEscrowWallet = await MultiSigWallet.new(ethAddresses, ethRequiredSigs);
-    // let etherEscrowAddress = await etherEscrowWallet.address;
-    // console.log("Ether Escrow Wallet" + " has been deployed to: " + etherEscrowAddress);
+    //General Setup
+    let etherEscrowWallet = await MultiSigWallet.new(ethAddresses, ethRequiredSigs);
+    let etherEscrowAddress = await etherEscrowWallet.address;
+    console.log("Ether Escrow Wallet" + " has been deployed to: " + etherEscrowAddress);
 
-    // let bountyWallet = await MultiSigWallet.new(bountyAddresses, bountyRequiredSigs);
-    // let bountyWalletAddress = await bountyWallet.address;
-    // console.log("bountyWallet" + " has been deployed to: " + bountyWalletAddress );
+    let bountyWallet = await MultiSigWallet.new(bountyAddresses, bountyRequiredSigs);
+    let bountyWalletAddress = await bountyWallet.address;
+    console.log("bountyWallet" + " has been deployed to: " + bountyWalletAddress );
 
-    // let foundersWallet = await MultiSigWallet.new(foundersAddresses, foundersRequiredSigs);
-    // let foundersAddress = await foundersWallet.address;
-    // console.log("Founders Wallet" + " has been deployed to: " + foundersAddress);
+    let foundersWallet = await MultiSigWallet.new(foundersAddresses, foundersRequiredSigs);
+    let foundersAddress = await foundersWallet.address;
+    console.log("Founders Wallet" + " has been deployed to: " + foundersAddress);
 
-    // let reserveWallet = await MultiSigWallet.new(reserveAddresses, reserveRequiredSigs);
-    // let reserveAddress = await reserveWallet.address;
-    // console.log("Reserve Wallet" + " has been deployed to: " + reserveAddress );
+    let reserveWallet = await MultiSigWallet.new(reserveAddresses, reserveRequiredSigs);
+    let reserveAddress = await reserveWallet.address;
+    console.log("Reserve Wallet" + " has been deployed to: " + reserveAddress );
 
-    // let trusteeWallet = await Trustee.new(shpAddress);
-    // let trusteeWalletAddress = await trusteeWallet.address;
-    // console.log("Trustee Wallet" + " has been deployed to: " + trusteeWalletAddress );
+    let trusteeWallet = await Trustee.new(shpAddress);
+    let trusteeWalletAddress = await trusteeWallet.address;
+    console.log("Trustee Wallet" + " has been deployed to: " + trusteeWalletAddress );
 
     
-    // let shpController = await SHPController.new(reserveAddress, foundersAddress);
-    // let shpControllerAddress = await shpController.address;
-    // console.log("SHPController" + " has been deployed to: " + shpControllerAddress);
+    let shpController = await SHPController.new(reserveAddress, foundersAddress);
+    let shpControllerAddress = await shpController.address;
+    console.log("SHPController" + " has been deployed to: " + shpControllerAddress);
+
+    //SHP Controller
+    await shpController.setContracts(shp.address, trusteeWalletAddress);
+
+    const ethRate = 300;
+    const minDiscount = web3.toWei(1500 / ethRate);
+    const firstTierDiscount = web3.toWei(10000 / ethRate);
+    const secondTierDiscount = web3.toWei(50000 / ethRate);
+    const thirdTierDiscount = web3.toWei(250000 / ethRate);
+    const minContribution = web3.toWei(100 / ethRate);
+    const maxContribution = web3.toWei(500000 / ethRate);
+    const shpRate = 5000;
+
+    let sharpeCrowdsale = await SharpeCrowdsale.new(
+        etherEscrowAddress,
+        bountyWalletAddress,
+        trusteeWalletAddress,
+        minDiscount,
+        firstTierDiscount,
+        secondTierDiscount,
+        thirdTierDiscount,
+        minContribution,
+        maxContribution,
+        shpRate
+    );
+
+    await sharpeCrowdsale.setShp(shpAddress, {
+        from: masterAddress
+    });
+
+    console.log("FOUNDERS MULTISIG ABI ARGUMENTS: ", 
+    abi.rawEncode(["address[]","uint"], [foundersAddresses,foundersRequiredSigs]).toString('hex'));
+
+    console.log("RESERVE MULTISIG ABI ARGUMENTS: ", 
+    abi.rawEncode(["address[]","uint"], [reserveAddresses,reserveRequiredSigs]).toString('hex'));
+
+    console.log("ETHER MULTISIG ABI ARGUMENTS: ", 
+    abi.rawEncode(["address[]","uint"], [ethAddresses,ethRequiredSigs]).toString('hex'));
+
+    console.log("BOUNTY MULTISIG ABI ARGUMENTS: ", 
+    abi.rawEncode(["address[]","uint"], [bountyAddresses,bountyRequiredSigs]).toString('hex'));
+
+    // Log all addresses:
+    console.log("__________________________________________")
+    console.log("********Completed Deploying Contracts*****")
+    console.log("__________________________________________")
+    console.log("Ether Escrow Wallet" + " has been deployed to: " + etherEscrowAddress);
+    console.log("bountyWallet" + " has been deployed to: " + bountyWalletAddress );
+    console.log("Founders Wallet" + " has been deployed to: " + foundersAddress);
+    console.log("Reserve Wallet" + " has been deployed to: " + reserveAddress );
+    console.log("Trustee Wallet" + " has been deployed to: " + trusteeWalletAddress );
+    console.log("SHPController" + " has been deployed to: " + shpControllerAddress);
+    console.log("SHP" + "has been deployed to: " + shpAddress);
+    console.log("__________________________________________");
+    console.log("__________________________________________");
+
+    console.log("__________________________________________");
+    console.log("********ABI ARGUMENTS*****");
+    console.log("__________________________________________");
+    console.log("SHP ABI ARGUMENTS: ", 
+    abi.rawEncode(["address"], [miniMeTokenFactory.address]).toString('hex'));
+   
+    console.log("TRUSTEE ABI ARGUMENTS: ", abi.rawEncode(["address"], [shpAddress]).toString('hex'));
+   
+    console.log("SHP Controller ABI ARGUMENTS: ", 
+    abi.rawEncode(["address","address"], [reserveAddress,foundersAddress]).toString('hex'));
+
+    console.log('Deployed the Sharpe crowdsale: ' + sharpeCrowdsale.address);
     
-    // let affiliateUtility = await AffiliateUtility.new(affiliateTierTwo, affiliateTierThree);
-    // let affiliateUtilityAddress = await affiliateUtility.address;
-    // console.log("AffiliateUtility" + " has been deployed to: " + affiliateUtilityAddress);
-
-    // //SHP Controller
-    // await shpController.setContracts(shp.address, trusteeWalletAddress);
-
-    // //Presale
-    // let presale = await SharpePresale.new(
-    //     etherEscrowAddress,
-    //     bountyWalletAddress,
-    //     trusteeWalletAddress,
-    //     affiliateUtilityAddress,
-    //     apiAddress,
-    //     web3.toWei(FIRST_TIER_DISCOUNT_UPPER_LIMIT / etherPeggedValue),
-    //     web3.toWei(SECOND_TIER_DISCOUNT_UPPER_LIMIT / etherPeggedValue),
-    //     web3.toWei(THIRD_TIER_DISCOUNT_UPPER_LIMIT / etherPeggedValue),
-    //     web3.toWei(MIN_PRESALE_CONTRIBUTION / etherPeggedValue),
-    //     web3.toWei(MAX_PRESALE_CONTRIBUTION / etherPeggedValue),
-    //     0,
-    //     honourWhitelistEnd
-    // );
-    // let presaleAddress = await presale.address;
-    // console.log("PreSale" + " has been deployed to: " + presaleAddress )
-
-    // // General Sale
-    // let minContributionInWei = web3.toWei(MIN_GENERAL_SALE_CONTRIBUTION / etherPeggedValue);
-   
-    // let generalSale = await SharpeCrowdsale.new(
-    //     etherEscrowAddress,
-    //     bountyWalletAddress,
-    //     trusteeWalletAddress,
-    //     affiliateUtilityAddress,
-    //     apiAddress,
-    //     minContributionInWei);
-    
-    // let generalSaleAddress = await generalSale.address;
-    // console.log("GeneralSale" + " has been deployed to: " + generalSaleAddress);
-
-    // let dynamicCeiling = await DynamicCeiling.new(masterAddress,generalSaleAddress);
-    // let dynamicCeilingAddress = await dynamicCeiling.address;
-
-    // console.log("DYNAMIC CEILIENG ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address","address"], [masterAddress,generalSaleAddress]).toString('hex'));
-
-    // console.log("FOUNDERS MULTISIG ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address[]","uint"], [foundersAddresses,foundersRequiredSigs]).toString('hex'));
-
-    // console.log("RESERVE MULTISIG ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address[]","uint"], [reserveAddresses,reserveRequiredSigs]).toString('hex'));
-
-    // console.log("ETHER MULTISIG ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address[]","uint"], [ethAddresses,ethRequiredSigs]).toString('hex'));
-
-    // console.log("BOUNTY MULTISIG ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address[]","uint"], [bountyAddresses,bountyRequiredSigs]).toString('hex'));
-
-    // await generalSale.setDynamicCeilingAddress(dynamicCeiling.address);
-    // console.log("Dynamic Ceiling" + " has been deployed to: " + dynamicCeilingAddress);
-
-    // // Log all addresses:
-    // console.log("__________________________________________")
-    // console.log("********Completed Deploying Contracts*****")
-    // console.log("__________________________________________")
-    // console.log("Ether Escrow Wallet" + " has been deployed to: " + etherEscrowAddress);
-    // console.log("bountyWallet" + " has been deployed to: " + bountyWalletAddress );
-    // console.log("Founders Wallet" + " has been deployed to: " + foundersAddress);
-    // console.log("Reserve Wallet" + " has been deployed to: " + reserveAddress );
-    // console.log("Trustee Wallet" + " has been deployed to: " + trusteeWalletAddress );
-    // console.log("SHPController" + " has been deployed to: " + shpControllerAddress);
-    // console.log("AffiliateUtility" + " has been deployed to: " + affiliateUtilityAddress);
-    // console.log("SHP" + "has been deployed to: " + shpAddress);
-    // console.log("PreSale" + " has been deplo;yed to: " + presaleAddress );
-    // console.log("GeneralSale" + " has been deployed to: " + generalSaleAddress);
-    // console.log("Dynamic Ceiling" + " has been deployed to: " + dynamicCeilingAddress);
-    // console.log("__________________________________________");
-    // console.log("__________________________________________");
-
-    // console.log("__________________________________________");
-    // console.log("********ABI ARGUMENTS*****");
-    // console.log("__________________________________________");
-    // console.log("SHP ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address"], [miniMeTokenFactory.address]).toString('hex'));
-   
-    // console.log("TRUSTEE ABI ARGUMENTS: ", abi.rawEncode(["address"], [shpAddress]).toString('hex'));
-   
-    // console.log("SHP Controller ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address","address"], [reserveAddress,foundersAddress]).toString('hex'));
-   
-    // // console.log("AFIILIATES ABI ARGUMENTS: ", 
-    // // abi.rawEncode(["uint256","uint256"], [affiliateTierTwo,affiliateTierThree]).toString('hex'));
-   
-    // console.log("PRESALE ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address",
-    // "address",
-    // "address",
-    // "address",
-    // "uint256",
-    // "uint256",
-    // "uint256",
-    // "uint256",
-    // "uint256",
-    // "uint256",
-    // "uint256"], 
-    // [etherEscrowAddress,
-    //     bountyWalletAddress,
-    //     trusteeWalletAddress,
-    //     affiliateUtilityAddress,
-    //     apiAddress,
-    //     web3.toWei(FIRST_TIER_DISCOUNT_UPPER_LIMIT / etherPeggedValue),
-    //     web3.toWei(SECOND_TIER_DISCOUNT_UPPER_LIMIT / etherPeggedValue),
-    //     web3.toWei(THIRD_TIER_DISCOUNT_UPPER_LIMIT / etherPeggedValue),
-    //     web3.toWei(MIN_PRESALE_CONTRIBUTION / etherPeggedValue),
-    //     web3.toWei(MAX_PRESALE_CONTRIBUTION / etherPeggedValue),
-    //     web3.toWei(PRESALE_CAP / etherPeggedValue),
-    //     honourWhitelistEnd
-    // ]).toString('hex'));
-    
-    // console.log("GENERALSALE ABI ARGUMENTS: ", 
-    // abi.rawEncode(["address",
-    // "address",
-    // "address",
-    // "address",
-    // "uint256"], 
-    // [etherEscrowAddress,
-    //     bountyWalletAddress,
-    //     trusteeWalletAddress,
-    //     affiliateUtilityAddress,
-    //     apiAddress,
-    //     minContributionInWei
-    // ]).toString('hex'));
+    console.log("Crowdsale ABI arguments: ", 
+    abi.rawEncode([
+        "address",
+        "address",
+        "address",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256"
+    ], [
+        etherEscrowAddress,
+        bountyWalletAddress,
+        trusteeWalletAddress,
+        minDiscount,
+        firstTierDiscount,
+        secondTierDiscount,
+        thirdTierDiscount,
+        minContribution,
+        maxContribution,
+        shpRate
+    ]).toString('hex'));
 };
